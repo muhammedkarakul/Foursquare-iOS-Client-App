2dc95b99a108a5c74ee5cc90308a6608acedb635
Muhammed Karakul <muhammed.karakul@loodos.com>
2021-01-25 12:23:20 +0300

    Inital commit.

---
+++ Foursquare iOS Client App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foursquare iOS Client App
+//
+//  Created by Muhammed Karakul on 25.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
