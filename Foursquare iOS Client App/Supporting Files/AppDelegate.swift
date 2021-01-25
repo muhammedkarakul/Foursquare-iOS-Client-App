00c9a17e73ffbaf3ad9f14c7a9d9fe2305fad2d1
Muhammed Karakul <muhammed.karakul@loodos.com>
2021-01-25 13:42:09 +0300

    Project files separated into new groups. SceneDelegate removed from project. iOS 14.3 support downgrade to iOS 9.0

---
+++ Foursquare iOS Client App/Supporting Files/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Foursquare iOS Client App
+//
+//  Created by Muhammed Karakul on 25.01.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
